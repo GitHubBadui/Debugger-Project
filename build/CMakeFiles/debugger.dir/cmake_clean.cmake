604767c656d6de41696ebb2f8ee38c5e54427e3d
eitanradinsky <eitanradinsky@gmail.com>
2026-09-24 18:12:41 +0300

    build: Added Cmake file and .gitignore

---
+++ build/CMakeFiles/debugger.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/debugger.dir/link.d"
+  "CMakeFiles/debugger.dir/main.cpp.o"
+  "CMakeFiles/debugger.dir/main.cpp.o.d"
+  "debugger"
+  "debugger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/debugger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
